6e7bb68581369d9ed8948ebb339016d31233936d
Yash Vaidya <bobbynaldocr7@gmail.com>
2024-08-30 00:30:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function makeid(l) {
   // write your code here
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+
+	for(let i=0; i<l; i++) {
+		const randow = Math.floor(Math.random() * characters.length);
+		result +=characters[randow];
+	}
+	return result;
 }
 
 // Do not change the code below.
